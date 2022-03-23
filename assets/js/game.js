a2efa5ea4b384786edb803c3d037c4f26754f614
Will G <wgrout87@gmail.com>
2022-03-23 17:58:06 -0600

    robot name bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -78,9 +78,6 @@ var startGame = function () {
             // reset enemy.health before starting new fight
             pickedEnemyObj.health = randomNumber(40, 60);
 
-            // use debugger to pause script from running and check what's going on at that moment in the code
-            // debugger;
-
             // pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
             fight(pickedEnemyObj);
 
@@ -165,8 +162,19 @@ var randomNumber = function (min, max) {
     return value;
 };
 
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?")
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
