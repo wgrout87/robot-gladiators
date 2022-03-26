4e5e8388bd3c0367df1da20ce7faa1b6a73f02b7
Will G <wgrout87@gmail.com>
2022-03-26 17:38:53 -0600

    high score tracking

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,7 +26,7 @@ var fightOrSkip = function () {
         }
     }
     return false;
-}
+};
 
 var fight = function (enemy) {
     // keep track of who goes first
@@ -120,11 +120,10 @@ var startGame = function () {
                     shop();
                 }
             }
-
-            else {
-                window.alert("You have lost your robot in battle! Game Over");
-                break;
-            }
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over");
+            break;
         }
     }
 
@@ -162,19 +161,28 @@ var shop = function () {
 };
 
 var endGame = function () {
-    // if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!");
+
+    // check localStorage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money than the high score, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     }
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
 
     // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
-        // restart the game
         startGame();
     }
     else {
@@ -198,7 +206,7 @@ var getPlayerName = function () {
 
     console.log("Your robot's name is " + name);
     return name;
-}
+};
 
 var playerInfo = {
     name: getPlayerName(),
